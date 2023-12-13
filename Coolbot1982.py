d3c9ff0de0720a54472aa593bedcc716fec693fb
nikitakuzmenko <nikita.kuzmenko08@gmail.com>
2023-12-13 20:06:27 +0300

    Coolbot1982 with code gpt (Я не могу нормально сделать :( )

--- Coolbot1982.py
+++ Coolbot1982.py
@@ -1,3 +1,11 @@
-print("Hello world!"
-      "gay")
+import telebot
 
+token = ("6736617509:AAHH0BVsvRn5fTpTxQWgw4T3CJGBZN5t6p4")
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(func=lambda message: message.text.lower() == 'привет')
+def handle_message(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+# Запуск бота
+bot.polling()
