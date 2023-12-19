1819f3ea63697add4bfb14a7871a787d23003e76
nikitakuzmenko <nikita.kuzmenko08@gmail.com>
2023-12-19 21:06:33 +0300

    YEAAAA

--- bot.py
+++ bot.py
@@ -1,36 +1,44 @@
 import telebot
 from info import information, help, visitochka
+from config import bottoken
+
+bot = bottoken
 
 
-bot = telebot.TeleBot(token="6736617509:AAGvsPRRdYPE8VIaoLYwl8q_fqaBMTRrO6M")
-
-def Secretfilter(message):
+def secretfilter(message):
     password = "12345"
     return password in message.text.lower()
+
 
 def Goodbye(message):
     key = "пока"
     return key in message.text.lower()
 
-@bot.message_handler(content_types=['text'], func=Secretfilter)
+
+@bot.message_handler(content_types=['text'], func=secretfilter)
 def secret_way(message):
     bot.send_message(message.chat.id, f"Здравствуй! Ты открыл секрет, прими мои поздравления!")
 
+
 @bot.message_handler(content_types=['text'], func=Goodbye)
-def bye(message):
+def say_bye(message):
     bot.send_message(message.chat.id, f"Эхх, прощай.")
+
 
 @bot.message_handler(commands=['start'])
 def reply(message):
     bot.send_message(message.chat.id, f"{information}")
 
+
 @bot.message_handler(commands=['help'])
-def helpcom(message):
+def get_help(message):
     bot.send_message(message.chat.id, f"{help}")
+
 
 @bot.message_handler(commands=['visitka'])
 def giveinfo(message):
     bot.send_message(message.chat.id, f"{visitochka}")
+
 
 @bot.message_handler(content_types=['text'])
 def send_media(message):
